6029de896ba65b9495e2fbfab5606b331bb32a4e
HostLis <hostlis@tmpk.net>
2022-12-05 00:56:06 +0300

    Реализован класс Post

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Привет мир");
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("HTTP")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        httpClient.close();
+
+        List<Post> posts = mapper.readValue(body, new TypeReference<>() {
+        });
+        List<Post> count = posts
+                .stream()
+                .filter(post -> post.getUpvotes() > 0)
+                .collect(Collectors.toList());
+        count.forEach(System.out::println);
     }
 }
